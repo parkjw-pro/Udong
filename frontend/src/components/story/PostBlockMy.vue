6c1648ee52e683b00057ce9dde642c467a057a1d
이규용 <gyuyong290@gmail.com>
2021-02-04 09:29:01 +0900

    story userpost 부분 수정

--- frontend/src/components/story/PostBlockMy.vue
+++ frontend/src/components/story/PostBlockMy.vue
@@ -1,24 +1,36 @@
 <template>
   <div>
     <!-- <b-card-group deck> -->
+      <div v-for="post in posts" :key="post.postId">
       <b-card header-tag="header" footer-tag="footer"> <!-- title="Title" 속성 사용 가능  -->
-        <template >
+      <!-- <div v-for="post in posts" v-bind:key="post.postId" @click="detail(post.postId)"> 
+        <p>{{post.nickname}}</p>
+        <br>
+        <p>{{post.postContent}}</p>
+      </div> -->
+        <template>
           <b-card-text class="font-weight-bold">
+            <!-- 뱃지 추가할 경우 backend에서 sql문 수정해서 뱃지 정보도 가져와야 함-->
+<!-- 좋아요, 댓글 아이콘 추가해둔 상태. 와이어프레임 참고해서 상세 페이지까지 수정할 것!!!-->
             <!-- <span class="mr-5">뱃지 img</span> -->
-            <!-- <span >용용이</span> -->
+            <span >{{post.nickname}}</span>
           </b-card-text>
         </template>
-        <div> <!-- for 문 -->
+        <div @click="detail(post)"> <!-- for 문 -->
           <!-- <b-card-img class="col-3 mb-5" src="https://placekitten.com/480/210" alt="Image" bottom></b-card-img> -->
           <ImageSlick />
+          <p>{{post.postContent}}</p>
         </div>
-        <p>게시글 내용</p>
         <template #footer>
-          <b-button class="mr-5" href="#" variant="info">상세보기</b-button>
-          <!-- <b-button class="ml-5" href="#" variant="info">댓글</b-button> -->
+          <b-icon icon="suit-heart" variant="danger"></b-icon>
+          <b-icon icon="suit-heart-fill" variant="danger"></b-icon>
+          <b-icon icon="chat"></b-icon>
+          <!-- <b-button class="mr-5" href="#" variant="info">상세보기</b-button> -->
+          <b-button class="ml-5" href="#" variant="info">댓글</b-button>
         </template>
       </b-card>
     <!-- </b-card-group> -->
+      </div>
    <div>
 </div>   
   </div>
@@ -26,6 +38,7 @@
 
 <script>
 import ImageSlick from '@/components/story/ImageSlick'
+import { mapGetters } from "vuex";
 import axios from 'axios';
 const SERVER_URL = "http://localhost:8000";
 
@@ -34,10 +47,13 @@ export default {
   components: {
     ImageSlick,
   },
+  computed: {
+    ...mapGetters(["getUserId"])
+  },
   data() {
     return {
       posts:[],
-      limit: 3,  //한 페이지에 노출될 게시글의 수
+      limit: 5,  //한 페이지에 노출될 게시글의 수
       offset: 0,  //게시글 번호 오프셋
     }
   },
@@ -47,15 +63,20 @@ export default {
   methods: {
     getPostInfos(){
       axios
-        .get(`${SERVER_URL}/userpost`, {
+        .get(`${SERVER_URL}/userpost/user`, {
           params: {
+            userId: this.getUserId,
             limit: this.limit,
             offset: this.offset
           }
       })
         .then((response) => {
-          console.log(response);
+          this.posts = response.data.list;
+          console.log(this.posts);
         });
+    },
+    detail(post){
+      this.$router.push({ name: "ArticleDetail", params: { postInfo: post} });
     }
   },
 }
--- frontend/src/views/story/ArticleDetail.vue
+++ frontend/src/views/story/ArticleDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 이미지 추가하는 부분을 넣어야 한다!!! -->
   <div id="article_box">
-    <b-row class="mb-5" align-h="between">
+    <b-row class="mb-5" align-h="between" v-if="groupInfo.groupId">
       <b-col><h5 class="font-weight-bold">그룹</h5></b-col>
       <b-col><h5 class="font-weight-bold">다이어트</h5></b-col>
     </b-row>
@@ -12,7 +12,7 @@
       <b-form-textarea
         class="mb-5"
         id="textarea"
-        placeholder="하... 진짜 이거 왜 이렇게 안 되는지 모르겠네... 어떻게 해야 될까!!! 누가 좀 알려줘!!!!! 미춰버릴거같아!!!!!!!!!!!!!!!!"
+        :placeholder="this.postInfo.postContent"
         rows="8"
         disabled
       ></b-form-textarea>
@@ -34,12 +34,13 @@
       <br>
       <br>
       <!-- 댓글 -->
-      <b-list-group style="text-align: left;">  <!-- for 문 -->
-        <b-list-group-item><span class="font-weight-bold">용용</span>  멋있어요  <span class="small">몇 분 전</span></b-list-group-item>
-        <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
-        <b-list-group-item>Morbi leo risus</b-list-group-item>
-        <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
-        <b-list-group-item>Vestibulum at eros</b-list-group-item>
+      <b-list-group style="text-align: left;" v-for="comment in comments" :key="comment.commentId">  <!-- for 문 -->
+        <b-list-group-item>
+          <span class="font-weight-bold">{{comment.nickname}}</span>  
+          {{comment.commContent}}  
+          <span class="small">{{comment.createdAt}}</span>
+        </b-list-group-item>
+
       </b-list-group>
      
       <!-- <b-row align-h="between" class="mx-5 mb-5">
@@ -59,14 +60,46 @@
 
 <script>
 import ImageSlick from '@/components/story/ImageSlick'
+import axios from 'axios';
 
 
+const SERVER_URL = "http://localhost:8000";
 
 export default {
   name: 'ArticleDetail',
   components: {
     ImageSlick
-  }
+  },
+  data() {
+    return {
+      postInfo: this.$route.params.postInfo,
+      comments: [],
+      limit: 5,
+      offset: 0,
+      groupInfo: {}
+    }
+  },
+  created() {
+    this.getArticleComments();
+  },
+  methods: {
+    getArticleComments(){
+      console.log(this.postInfo.postId);
+      axios
+        .get(`${SERVER_URL}/userpost/comment`, {
+          params: {
+            postId: this.postInfo.postId,
+            limit: this.limit,
+            offset: this.offset
+          }
+        })
+        .then(
+          (response) => (
+            this.comments = response.data.list
+          )
+        );
+    }
+  },
 }
 
 
