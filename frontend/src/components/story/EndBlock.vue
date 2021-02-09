620a46d58b98a76fb0d472e9c6be46ce783dbf69
이규용 <gyuyong290@gmail.com>
2021-02-09 11:41:00 +0900

    FE group post comment, post 더보기 수정

--- frontend/src/components/story/EndBlock.vue
+++ frontend/src/components/story/EndBlock.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="mx-5 mt-5">
-    <img alt="Vue logo" src="@/assets/udonge.png" style="width: 10%">
+    <img alt="Vue logo" src="@/assets/udonge.png" style="width: 10%" @click="trigger">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'EndBlock'
+  name: 'EndBlock',
+  methods: {
+    trigger() {
+      this.$emit('more');
+    }
+  },
 }
 </script>
 
