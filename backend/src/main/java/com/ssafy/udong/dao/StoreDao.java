adcfe631519813e7370a4891a093fafc48030934
Park Jongwon <whddnjs18212@naver.com>
2021-02-15 13:25:59 +0900

    리뷰수정0215

--- backend/src/main/java/com/ssafy/udong/dao/StoreDao.java
+++ backend/src/main/java/com/ssafy/udong/dao/StoreDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.udong.dto.StoreDto;
+import com.ssafy.udong.dto.StoreParamDto;
 @Mapper
 public interface StoreDao {
 	
@@ -15,5 +16,13 @@ public interface StoreDao {
 	public List<StoreDto> SelectArea(String searchWord);
 
 	public StoreDto selectStoreDetail(String storeId);
+
+	public List<String> selectBestCtg(String dongCode);
+	
+	public List<String> selectBestStore(String dongCode);
+
+	public List<StoreDto> selectBestCtgStore(StoreParamDto storeParamDto);
+
+	public List<StoreDto> selectBestStore(StoreParamDto storeParamDto);
 
 }
