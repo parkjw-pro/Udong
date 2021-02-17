b6c832808fd1c14805954b2cb88580c014ff3c9a
이송영 <bulgen@naver.com>
2021-02-17 21:37:44 +0900

    song: badge

--- frontend/src/views/app/Badge.vue
+++ frontend/src/views/app/Badge.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="box">
     <h3>{{ nickname }}<small>님의 뱃지</small></h3>
+    <br>
     <b-row class="mt-5">
       <b-col class="badge">
         <img
           @click="mainBadge(1)"
           v-bind:class="[badgecount > 0 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge1.jpg"
         />
         <p class="mt-2">열정적인 노동자</p>
@@ -15,19 +16,19 @@
         <img
           @click="mainBadge(2)"
           v-bind:class="[badgecount > 1 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge2.jpg"
         />
-        <p class="mt-2 unacquired_text">우동 영웅</p>
+        <p class="mt-2">우동 영웅</p>
       </b-col>
       <b-col class="badge">
         <img
           @click="mainBadge(3)"
           v-bind:class="[badgecount > 2 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge3.jpg"
         />
-        <p class="mt-2 unacquired_text">우동 귀요미</p>
+        <p class="mt-2">우동 귀요미</p>
       </b-col>
     </b-row>
     <b-row>
@@ -35,28 +36,28 @@
         <img
           @click="mainBadge(4)"
           v-bind:class="[badgecount > 3 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge4.jpg"
         />
-        <p class="mt-2 unacquired_text">코끼끼끼리리리</p>
+        <p class="mt-2">코끼끼끼리리리</p>
       </b-col>
       <b-col class="badge">
         <img
           @click="mainBadge(5)"
           v-bind:class="[badgecount > 4 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge5.jpg"
         />
-        <p class="mt-2 unacquired_text">창과 방패와의 싸움</p>
+        <p class="mt-2">창과 방패와의 싸움</p>
       </b-col>
       <b-col class="badge">
         <img
           @click="mainBadge(6)"
           v-bind:class="[badgecount > 5 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge6.jpg"
         />
-        <p class="mt-2 unacquired_text">무지개떡</p>
+        <p class="mt-2">무지개떡</p>
       </b-col>
     </b-row>
     <b-row>
@@ -64,39 +65,39 @@
         <img
           @click="mainBadge(7)"
           v-bind:class="[badgecount > 5 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge7.jpg"
         />
-        <p class="mt-2 unacquired_text">리뷰왕!</p>
+        <p class="mt-2">리뷰왕!</p>
       </b-col>
       <b-col class="badge">
         <img
           @click="mainBadge(8)"
           v-bind:class="[badgecount > 5 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge8.jpg"
         />
-        <p class="mt-2 unacquired_text">21번 도로의 지배자</p>
+        <p class="mt-2">21번 도로의 지배자</p>
       </b-col>
       <b-col class="badge">
         <img
           @click="mainBadge(9)"
           v-bind:class="[badgecount > 5 ? acquired : '', unacquired]"
-          alt="Vue logo"
+          alt="Badge"
           src="@/assets/app/badge/badge9.jpg"
         />
-        <p class="mt-2 unacquired_text">우동 큐피터</p>
+        <p class="mt-2">우동 큐피터</p>
       </b-col>
     </b-row>
     <b-modal v-model="modalShow" @ok="setmainBadge">
       <p>
-        <img alt="Vue logo" src="@/assets/udonge.png" style="width: 10%" />
+        <img alt="Badge" src="@/assets/udonge.png" style="width: 10%" />
         메인벳지로 하시겠습니까??
       </p>
     </b-modal>
     <b-modal v-model="modalShow2">
       <p>
-        <img alt="Vue logo" src="@/assets/udonge.png" style="width: 10%" />
+        <img alt="Badge" src="@/assets/udonge.png" style="width: 10%" />
         활동이 부족해요!!
       </p>
     </b-modal>
@@ -105,9 +106,13 @@
 
 <script>
 import axios from 'axios';
+
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
+
 export default {
   name: 'Badge',
+  components: {
+  },
   data: function() {
     return {
       unacquired: 'unacquired',
@@ -121,10 +126,10 @@ export default {
       modalShow2: false,
     };
   },
-  created() {
+  async created() {
     this.nickname = JSON.parse(localStorage.getItem('Info-token'))['nickname'];
     this.userId = JSON.parse(localStorage.getItem('Info-token'))['userId'];
-    this.selectBadge();
+    await this.selectBadge();
   },
   methods: {
     selectBadge() {
@@ -136,9 +141,9 @@ export default {
           },
         })
         .then((response) => {
-          console.log(response.data);
           this.badgeList = response.data;
           this.badgecount = response.data.length;
+          alert('대표뱃지 변경이 완료되었습니다! \n왼쪽 메뉴에서 확인해주세요!')
         });
     },
 
@@ -163,8 +168,6 @@ export default {
           localStorage.setItem('Login-token', JSON.stringify(userInfo));
           console.log(this.badgecount);
           location.replace(true);
-          // location.href='abc.php'
-          // location.replace();
         });
     },
   },
@@ -194,7 +197,7 @@ export default {
   filter: brightness(100%);
 }
 
-.unacquired_text {
+ {
   color: #cec4ea;
 }
 </style>
